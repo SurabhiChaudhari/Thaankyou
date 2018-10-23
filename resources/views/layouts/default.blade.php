133e2483cd3b9141709f7ffa3f903161249737ec
Surabhi Chaudhari <sc2263@njit.edu>
2018-10-23 16:52:28 -0400

    Feature1:Adding Jumbotron code

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+
+@include('includes.head')
+<body>
+@include('includes.menu')
+<main roles="main">
+    @yield('content')
+
+</main>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+@include('includes.jsfoooter')
+@include('includes.footer')
+
+</body>
+</html>
